abf06245c0fcdc454b84470b930f28d5e06ddf3c
Damien Rubio <rubio.damien@gmail.com>
2018-12-09 17:08:11 -0500

    fix component

--- damienrubio/src/app/experiences/jobs/jobs.component.ts
+++ damienrubio/src/app/experiences/jobs/jobs.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ExperiencesComponent } from '../experiences.component';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
   styleUrls: ['../experiences.component.css', './jobs.component.css']
 })
-export class JobsComponent implements OnInit {
+export class JobsComponent extends ExperiencesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
   ngOnInit() {
   }
