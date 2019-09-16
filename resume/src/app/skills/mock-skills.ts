c3c0a964bf060a02307f68b2908a8e9d33331fbd
Damien <rubio.damien@gmail.com>
2019-09-16 12:23:04 -0400

    minor ui changes

--- resume/src/app/skills/mock-skills.ts
+++ resume/src/app/skills/mock-skills.ts
@@ -28,7 +28,7 @@ export const SKILLS: Skill[] = [
     { name: 'Git / SVN', levelBase100: 90, order: 15, categoryId: 3 },
     { name: 'Github', levelBase100: 90, order: 15, categoryId: 3 },
     // { name: 'AWS', levelBase100: 20, order: 0, categoryId: 3 },
-    { name: 'Google Cloud', levelBase100: 10, order: 200, categoryId: 3 },
+    // { name: 'Google Cloud', levelBase100: 10, order: 200, categoryId: 3 },
     { name: 'Docker', levelBase100: 80, order: 0, categoryId: 3 },
     { name: 'Jenkins', levelBase100: 90, order: 100, categoryId: 3 },
     { name: 'Maven', levelBase100: 100, order: 100, categoryId: 3 },
@@ -37,12 +37,10 @@ export const SKILLS: Skill[] = [
     { name: 'Jib', levelBase100: 90, order: 0, categoryId: 3 },
     { name: 'Kaniko', levelBase100: 90, order: 0, categoryId: 3 },
     //
-    { name: 'Unix', levelBase100: 90, order: 0, categoryId: 4 },
-    { name: 'Windows', levelBase100: 90, order: 0, categoryId: 4 },
+    // { name: 'Unix', levelBase100: 90, order: 0, categoryId: 4 },
+    // { name: 'Windows', levelBase100: 90, order: 0, categoryId: 4 },
     //
-    { name: 'MySQL', levelBase100: 95, order: 0, categoryId: 5 },
-    { name: 'T/SQL', levelBase100: 95, order: 0, categoryId: 5 },
-    { name: 'PostgreSQL', levelBase100: 90, order: 0, categoryId: 5 },
+    { name: 'SQL', levelBase100: 95, order: 0, categoryId: 5 },
     { name: 'MongoDB', levelBase100: 70, order: 0, categoryId: 5 }
 ];
 
@@ -50,6 +48,6 @@ export const SKILL_CATEGORIES: SkillCategory[] = [
     { id: 1, name: 'Architecture / Methodology', name_fr: 'Architecture / Méthodologie', order: 0 },
     { id: 2, name: 'Languages / Frameworks', name_fr: 'Langages / Frameworks', order: 0 },
     { id: 3, name: 'Industrialization / Platforms', name_fr: 'Industrialisation / Plateformes', order: 0 },
-    { id: 4, name: 'OS', name_fr: 'OS', order: 0 },
+    // { id: 4, name: 'OS', name_fr: 'OS', order: 10 },
     { id: 5, name: 'Databases', name_fr: 'Bases de données', order: 0 }
 ];
