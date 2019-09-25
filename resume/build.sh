b5ed43f2e82e8fd8fdc7e64726955c74ccbbe236
Damien Rubio <rubio.damien@gmail.com>
2019-09-25 18:48:07 +0200

    Update resume.yml Update resume.yml Update resume.yml Update resume.yml

--- resume/build.sh
+++ resume/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$(dirname "$(readlink -f "$0")")"
+
+docker login --username ${DOCKER_USERNAME} --password ${DOCKER_TOKEN}
 docker build -t damru/web-resume ${DIR}
 docker push damru/web-resume
